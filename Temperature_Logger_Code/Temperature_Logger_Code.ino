7120320d4f1a9ea2ee2470cc0a83491afb44c112
QuentinRauschenbach <quentin.g.j.r@gmail.com>
2023-03-01 16:10:11 +0100

    Add libraries

---
+++ Temperature_Logger_Code/Temperature_Logger_Code.ino
@@ -0,0 +1,30 @@
+/***********************************
+ * author: Quentin Rauschenbach
+ * 
+ * email: quentin.g.j.r@gmail.com
+ * 
+ * UHH WiSe 2022/2023
+ * Practical Measurement Electronics and Interfaces in ocean Sciences
+ * 
+ * This script uses an Arduino together with a DS18B20 temperature
+ * sensor and pure OneWire based communication.
+ * 
+ ***********************************/
+
+#include <SD.h>
+#include <SPI.h>
+#include <RTClib.h>
+#include <OneWire.h>
+//#include <DallasTemperature.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
