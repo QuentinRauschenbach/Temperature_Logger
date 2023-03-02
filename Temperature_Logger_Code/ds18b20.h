34c8c30b1973b066c4f5b5bd146a4dce56b86936
QuentinRauschenbach <quentin.g.j.r@gmail.com>
2023-03-02 14:01:25 +0100

    Read and print spot Temperature and sensor ID

--- Temperature_Logger_Code/ds18b20.h
+++ Temperature_Logger_Code/ds18b20.h
@@ -3,3 +3,7 @@
  */
 
 #define CONVERT_T 0x44 // out source this into helper file
+#define READ_ROM 0x33 //
+#define IS_DS18B20_SENSOR 0x28// family code
+#define SKIP_ROM 0xCC
+#define READ_SCRATCHPAD 0xBE //
